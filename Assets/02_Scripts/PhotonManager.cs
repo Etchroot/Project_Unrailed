38727e6a8dbec105b58156dfb49ba1f276ecb568
ChaMyeongGeun <jjamyeong@gamil.com>
2024-11-18 11:25:36 +0900

    Photon Script

--- Assets/02_Scripts/PhotonManager.cs
+++ Assets/02_Scripts/PhotonManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
+using UnityEngine.UI;
 
 
 public class PhotonManager : MonoBehaviourPunCallbacks
@@ -9,6 +11,20 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     [SerializeField] private const string version = "1.0";
 
     private string nickname = "Cha";
+
+    [Header("Text")]
+    [SerializeField] private TMP_InputField nickNameIF;
+    [SerializeField] private TMP_InputField roomNameIF;
+
+    [Header("Button")]
+    [SerializeField] private Button makeRoomButton;
+    [SerializeField] private Button OptionButton;
+    [SerializeField] private Button loginButton;
+
+    [Header("Room List")]
+    [SerializeField] private GameObject roomPrefab;
+    [SerializeField] private Transform contenTR;
+
 
     private void Awake()
     {
@@ -24,6 +40,52 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     void Start()
     {
         nickname = PlayerPrefs.GetString("NICK_NAME", $"USER_{Random.Range(0, 1001):0000}");
+        nickNameIF.text = nickname;
+
+        loginButton.onClick.AddListener(() => OnLoginButtonClick());
+        makeRoomButton.onClick.AddListener(() => OnMakeRoomButtonClick());
+        OptionButton.onClick.AddListener(() => VisibleOption());
+    }
+
+    private void VisibleOption()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    private void OnLoginButtonClick()
+    {
+        SetNickNam();
+        PhotonNetwork.JoinRandomRoom();
+    }
+    private void OnMakeRoomButtonClick()
+    {
+        SetNickNam();
+        if (string.IsNullOrEmpty(roomNameIF.text))
+        {
+            roomNameIF.text = $"ROOM_{Random.Range(0, 1000)}";
+        }
+        var roomOptions = new RoomOptions
+        {
+            MaxPlayers = 2,
+            IsOpen = true,
+            IsVisible = true
+        };
+        PhotonNetwork.CreateRoom(roomNameIF.text, roomOptions);
+    }
+    private void SetNickNam()
+    {
+        if (string.IsNullOrEmpty(nickNameIF.text))
+        {
+            nickname = $"USER_{Random.Range(0, 1001):0000}";
+            nickNameIF.text = nickname;
+        }
+        else
+        {
+            nickname = nickNameIF.text;
+        }
+        PhotonNetwork.NickName = nickname;
+        PlayerPrefs.SetString("NICK_NAME", nickname);
     }
 
     #region 포톤 콜백 함수
