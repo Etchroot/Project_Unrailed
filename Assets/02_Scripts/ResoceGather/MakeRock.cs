9ddab1d767de8a45b5e3fac055cf3c32e8a44e43
Tak <blueligh@naver.com>
2024-11-20 17:53:47 +0900

    photon prepare

--- Assets/02_Scripts/ResoceGather/MakeRock.cs
+++ Assets/02_Scripts/ResoceGather/MakeRock.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using UnityEngine;
 
 
@@ -5,6 +6,12 @@ public class MakeRock : MonoBehaviour
 {
     private int hp = 3;
     public GameObject stone;
+    PhotonView photonView;
+
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
 
     public int HP
     {
@@ -16,14 +23,19 @@ public class MakeRock : MonoBehaviour
         {
             hp = value;
             Debug.Log($" {hp}");
-            if (hp <= 0)
+            if (hp <= 0
+            //&& photonView.IsMine
+            )
             {
+                //PhotonNetwork.Instantiate("Stone", this.transform.position + Vector3.up * 2.2f, Quaternion.identity);
                 Instantiate(stone, this.transform.position + Vector3.up * 2.2f, Quaternion.identity);
+                //PhotonNetwork.Destroy(this.gameObject);
                 Destroy(this.gameObject);
             }
         }
     }
 
+    //[PunRPC]
     public void Doit(int v)
     {
         HP -= v;
