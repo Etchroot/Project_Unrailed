4e3b018e1adaa1abc29d265c5c4ad0e7bbc026dc
Cha MG <jjamyeong@gmail.com>
2024-11-21 16:37:09 +0900

    Fix RailSelecUI

--- Assets/02_Scripts/InstantiateRail.cs
+++ Assets/02_Scripts/InstantiateRail.cs
@@ -1,16 +1,44 @@
+using System;
+using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InstantiateRail : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Button[] railbuttons; // 3개의 버튼 배열
+    [SerializeField] private GameObject[] railPrefab; // rail 프리펩 3개 배열
+    private Transform instantPoint; // 레일 생성 위치
+
     void Start()
     {
-        
+    }
+    public void SetRail(Vector3 target)
+    {
+        for (int i = 0; i < railbuttons.Length; i++)
+        {
+            int index = i; // Lambda 캡쳐문제 방지
+            if (railbuttons != null)
+            {
+                railbuttons[i].onClick.AddListener(() => instantRailPrefab(index, target));
+            }
+        }
+    }
+    private void instantRailPrefab(int index, Vector3 target)
+    {
+        if (railPrefab != null && index < railPrefab.Length && railPrefab[index] != null)
+        {
+            Instantiate(railPrefab[index], target, Quaternion.identity);
+
+        }
+        else
+        {
+            Debug.Log("레일 프리펩이나 인덱스 존재하지 않음");
+
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/02_Scripts/RaytoTile.cs
+++ Assets/02_Scripts/RaytoTile.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR.OpenXR.NativeTypes;
 using UnityEngine.XR;
+using UnityEngine.Subsystems;
 
-public class InstallRail : MonoBehaviour
+public class RaytoTile : MonoBehaviour
 {
     [SerializeField] private GameObject RayPos; // 레이 발사 지점
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -21,25 +21,35 @@ public class InstallRail : MonoBehaviour
 
     void FindPossibleTile()
     {
+        InstantiateRail instantiateRail = FindAnyObjectByType<InstantiateRail>();
         Ray ray = new Ray(RayPos.transform.position, RayPos.transform.forward);
         RaycastHit hitinfo;
+
+        // 레이 디버그 선 그리기
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 2f);
 
         if (Physics.Raycast(ray, out hitinfo))
         {
             if (hitinfo.collider.tag == "EMPTY")
             {
-                selectRail();
+                Vector3 hitposition = hitinfo.point;
+                instantiateRail.SetRail(hitposition);
+                hitinfo.collider.gameObject.tag = "INSTALL";
+            }
+            else
+            {
+                Debug.Log("설치할 수 없는 타일입니다.");
             }
         }
     }
 
-    public void selectRail()
-    {
+    // public void selectRail()
+    // {
 
-    }
+    // }
 
-    public void selectUI()
-    {
+    // public void selectUI()
+    // {
 
-    }
+    // }
 }
