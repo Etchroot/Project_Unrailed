55f924bc4ef1d4d482d4a8cab13b867f691b4624
Cha MG <jjamyeong@gmail.com>
2024-11-21 17:57:00 +0900

    Instantiate Rail OK

--- Assets/02_Scripts/RaytoTile.cs
+++ Assets/02_Scripts/RaytoTile.cs
@@ -4,10 +4,14 @@ using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR.OpenXR.NativeTypes;
 using UnityEngine.XR;
 using UnityEngine.Subsystems;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class RaytoTile : MonoBehaviour
 {
-    [SerializeField] private GameObject RayPos; // 레이 발사 지점
+    [SerializeField] private Transform Controller; // 레이 발사 지점
+    [SerializeField] private float rayLength = 100f; // 레이 길이
+    [SerializeField] private InputActionProperty triggerAcrion; // 트리거 버튼 액션
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,29 +20,38 @@ public class RaytoTile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FindPossibleTile();
+    }
+
+    void OnEnable()
+    {
 
     }
 
     void FindPossibleTile()
     {
         InstantiateRail instantiateRail = FindAnyObjectByType<InstantiateRail>();
-        Ray ray = new Ray(RayPos.transform.position, RayPos.transform.forward);
+        Ray ray = new Ray(Controller.transform.position, Controller.transform.forward);
         RaycastHit hitinfo;
 
         // 레이 디버그 선 그리기
-        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 2f);
+        Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red, 2f);
 
-        if (Physics.Raycast(ray, out hitinfo))
+        if (triggerAcrion.action.WasPerformedThisFrame())
         {
-            if (hitinfo.collider.tag == "EMPTY")
+
+            if (Physics.Raycast(ray, out hitinfo, rayLength))
             {
-                Vector3 hitposition = hitinfo.point;
-                instantiateRail.SetRail(hitposition);
-                hitinfo.collider.gameObject.tag = "INSTALL";
-            }
-            else
-            {
-                Debug.Log("설치할 수 없는 타일입니다.");
+                if (hitinfo.collider.tag == "EMPTY")
+                {
+                    Vector3 hitposition = hitinfo.point;
+                    instantiateRail.SetRail(hitposition);
+                    hitinfo.collider.gameObject.tag = "INSTALL";
+                }
+                else
+                {
+                    Debug.Log("설치할 수 없는 타일입니다.");
+                }
             }
         }
     }
