d4b569b4405cd7b29f8c557dac89c2c88a5f87d9
Tak <blueligh@naver.com>
2024-11-21 11:54:55 +0900

    train controll rod lever

---
+++ Assets/02_Scripts/Train/trainRod.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class trainRod : MonoBehaviour
+{
+    Animator anim;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Doit()
+    {
+        anim.SetTrigger("Doit");
+    }
+
+}
