42f603874c8c870e7f469f4e1f20e02cbf8d6dc4
Cha MG <jjamyeong@gmail.com>
2024-11-25 14:31:20 +0900

    Rail bug fix

--- Assets/02_Scripts/RaytoTile.cs
+++ Assets/02_Scripts/RaytoTile.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR.OpenXR.NativeTypes;
 using UnityEngine.XR;
@@ -12,6 +13,8 @@ public class RaytoTile : MonoBehaviour
     [SerializeField] private Transform Controller; // 레이 발사 지점
     [SerializeField] private float rayLength = 100f; // 레이 길이
     [SerializeField] private InputActionProperty triggerAcrion; // 트리거 버튼 액션
+    [SerializeField] private Button[] railbuttons; // 3개의 버튼 배열
+    [SerializeField] private GameObject[] railPrefab; // rail 프리펩 3개 배열
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,7 +33,7 @@ public class RaytoTile : MonoBehaviour
 
     void FindPossibleTile()
     {
-        InstantiateRail instantiateRail = FindAnyObjectByType<InstantiateRail>();
+        //InstantiateRail instantiateRail = FindAnyObjectByType<InstantiateRail>();
         Ray ray = new Ray(Controller.transform.position, Controller.transform.forward);
         RaycastHit hitinfo;
 
@@ -48,7 +51,7 @@ public class RaytoTile : MonoBehaviour
                     Debug.Log("empty타일");
                     Vector3 hitposition = hitinfo.collider.bounds.center;
                     hitinfo.collider.gameObject.tag = "INSTALL";
-                    instantiateRail.SetRail(hitposition);
+                    SetRail(hitposition);
                 }
                 else
                 {
@@ -57,15 +60,38 @@ public class RaytoTile : MonoBehaviour
                 }
             }
         }
+
+    }
+    public void SetRail(Vector3 target)
+    {
+        for (int i = 0; i < railbuttons.Length; i++)
+        {
+            int index = i; // Lambda 캡쳐문제 방지
+            if (railbuttons != null)
+            {
+                Debug.Log("set rail 도달");
+                railbuttons[i].onClick.AddListener(() => instantRailPrefab(index, target));
+            }
+            else
+            {
+                Debug.Log("set rail 도달 실패");
+            }
+        }
+    }
+    private void instantRailPrefab(int index, Vector3 target)
+    {
+        if (railPrefab != null && index < railPrefab.Length && railPrefab[index] != null)
+        {
+            Debug.Log("프리펩 생성 도달");
+            Instantiate(railPrefab[index], target, Quaternion.identity);
+
+        }
+        else
+        {
+            Debug.Log("레일 프리펩이나 인덱스 존재하지 않음");
+
+        }
     }
 
-    // public void selectRail()
-    // {
 
-    // }
-
-    // public void selectUI()
-    // {
-
-    // }
 }
